50c06146cca2ce98b43d186ef1a59c03a13282ed
Anonymous <a@b.com>
2020-06-26 07:58:29 +0000

    -

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import re
 import setuptools
 
-from jugaad_data import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,7 +11,7 @@ with open("src/flask/__init__.py", encoding="utf8") as f:
 
 setuptools.setup(
     name="Jugaad Data", # Replace with your own username
-    version=__version__,
+    version=version,
     description="Jugad data is a library to download historical price-volume data from NSE in pandas dataframe.",
     long_description=long_description,
     long_description_content_type="text/markdown",
